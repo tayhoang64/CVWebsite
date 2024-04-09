2abb690f5c11218ba3aee7c3ed5a0e0ed860f11f
Vu Nguyen <vunguyen10a8.nt@gmail.com>
2024-04-09 23:10:24 +0700

    CRUD Templates

---
+++ WebCV/Migrations/20240408050952_V1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebCV.Migrations
+{
+    /// <inheritdoc />
+    public partial class V1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Link",
+                table: "Template",
+                type: "VARCHAR(255)",
+                unicode: false,
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1)",
+                oldUnicode: false,
+                oldMaxLength: 1,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Link",
+                table: "Company",
+                type: "VARCHAR(255)",
+                unicode: false,
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1)",
+                oldUnicode: false,
+                oldMaxLength: 1,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Link",
+                table: "Template",
+                type: "varchar(1)",
+                unicode: false,
+                maxLength: 1,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(1)",
+                oldUnicode: false,
+                oldMaxLength: 1,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Link",
+                table: "Company",
+                type: "varchar(1)",
+                unicode: false,
+                maxLength: 1,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(1)",
+                oldUnicode: false,
+                oldMaxLength: 1,
+                oldNullable: true);
+        }
+    }
+}
---
+++ WebCV/Migrations/20240409021624_V2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebCV.Migrations
+{
+    /// <inheritdoc />
+    public partial class V2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Template",
+                type: "varchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Template");
+        }
+    }
+}
